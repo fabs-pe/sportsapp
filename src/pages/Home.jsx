bb24f6d1c576fcae4a6ec1373f5195b36ca42679
Fabian <128451994+fabs-pe@users.noreply.github.com>
2025-08-04 22:12:51 +1000

    wip: notice

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,13 @@ import '../App.css';
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center pt-24">
-        <h1 className='text-black-900 md:text-8xl lg:text-6xl text-center'>Sports</h1>
+        <h1 className='text-black-900 md:text-8xl lg:text-6xl text-center'>All Sports Info</h1>
         <p className='w-4/6 antialiased align-middle text-center pt-12'>
         This app utilizes the SportsDB API for all its data. It's a project designed to showcase my React
         skills and practice Tailwind CSS.</p>
+        <p className='text-blue-600'>
+          More Features Too Come
+        </p>
       
    </div>
   );
